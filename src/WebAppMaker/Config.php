155f7206672d57030c59291e1c87fa6c77743d31
Thomas Chartron <thomas.chartron@gmail.com>
2019-01-27 18:37:23 +0100

    :construction: Read config file and instantiate config var array ready to build app

--- src/WebAppMaker/Config.php
+++ src/WebAppMaker/Config.php
@@ -4,20 +4,54 @@ namespace WebAppMaker;
 
 class Config {
 
+    // public const __ROOT_DIR__ = __DIR__;
+
+    private $allowedExtension = ["conf", "appconf"];
+    private $climateInstance;
+
+    public function __construct($climateInstance)
+    {
+        $this->climateInstance = $climateInstance;
+    }
+
+    public function selectConfigFile($configPath) : string
+    {
+        //trim trailing slash
+        $configPath = rtrim($configPath, "/");
+        $brace = "{".implode(",", $this->allowedExtension)."}"; //{jpg,gif,png}
+        $files = glob("$configPath/*.$brace", GLOB_BRACE);
+        // $this->climateInstance->backgroundLightGreen()->bold()->black()->out("$configPath/*.$brace");
+        // die(var_dump($files));
+        // $options = ['Ice Cream', 'Mixtape', 'Teddy Bear', 'Pizza', 'Puppies'];
+        $input = $this->climateInstance->radio('Please send me one of the following:', $files);
+        $response = $input->prompt();
+        return $response;
+    }
+
     public function readConfigFile($filePath) : array
     {
-        $args = [];
+        $config = [];
         if ($file = fopen($filePath, "r")) {
             while(!feof($file)) {
                 $line = fgets($file);
-                die($line);
+                //convert all values to key value pair
+                try {
+                    $explodeLine = explode("=", $line);
+                    //push to array
+                    if(!empty($explodeLine[0])) {
+                        $config[$explodeLine[0]] = $explodeLine[1];
+                    }
+                } catch (Exception $ex) {
+                    $this->climateInstance->bold()->red()->out("Failed to read config file ".$ex->getMessage());
+                    exit();
+                }
             }
             fclose($file);
         }
-        return $args;
+        return array_filter($config);
     }
 
-    public function configApp($args) : bool
+    public function configureApp($args) : bool
     {
         //Make all web app config
         return false;

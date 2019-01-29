af9f82d25c6d0100eade95cef23468fdf6449e38
Thomas Chartron <thomas.chartron@gmail.com>
2019-01-29 23:28:41 +0100

    :construction: Getting vhost file parsing it to find vars to replace starting to replace with values

--- src/WebAppMaker/Configuration/Config.php
+++ src/WebAppMaker/Configuration/Config.php
@@ -64,10 +64,12 @@ class Config {
         }
 
         //Step by step instead of loop through parameters values
-        //Create app dev folder
-        // die($values['USER_FOLDER']);
-        // die(var_dump(file_exists($values['USER_FOLDER'])));
-        // die(var_dump(is_dir(trim($values['USER_FOLDER']))));
+        //Create app dev folder APP_FOLDER
+        //set owner and groups to APP_FOLDER_OWNER:APP_FOLDER_GROUP
+        //check APP_WEBSERVER
+        //create vhost VHOST_FILENAME
+        //fill VHOST_FILENAME
+        //
         /**
          * IMPORTANT NOTE :
          * Trim values is very important as it appear to have a CLRF/LF or a space and is_dir does not like this
@@ -85,11 +87,85 @@ class Config {
         } else {
             $this->climateInstance->bold()->red()->out($values['USER_FOLDER'].' USER_FOLDER Incorrect value or not a directory');
         }
-        //Create vhost
-        //Fill vhost
+
+        /////////////////////////////////////////
+        //WEB SERVER SPECIFIQUE CONFIGURATIONS //
+        /////////////////////////////////////////
+        if($this->noValueCheck($values['APP_WEBSERVER'])) {
+            switch(trim($values['APP_WEBSERVER'])) {
+                case 'apache2':
+                        //Create vhost file
+                        if($this->noValueCheck($values['VHOST_FILENAME'])) {
+                            $this->climateInstance->backgroundLightGreen()->bold()->black()->out('Creating vhost file  : '.$values['VHOST_FILENAME'].' in /etc/apache2/sites-available/');
+                            $this->execOrFail("touch  /etc/apache2/sites-available/".trim($values['VHOST_FILENAME']));
+                        }
+                        //Fill vhost
+                        //trim trailing slash
+                        // die(var_dump(TEST)); //Access constant
+                        $vhostPath = realpath(__DIR__."/../../../config/vhost/apache2");
+                        // $this->climateInstance->bold()->red()->out($vhostPath);
+                        // die(var_dump($vhostPath));
+                        $vhostPath = rtrim($vhostPath, "/");
+                        // $brace = "{".implode(",", $this->allowedExtension)."}"; //{jpg,gif,png}
+                        // die(realpath(__DIR__."../../config/vhost/apache2/"));
+                        $files = glob($vhostPath."/*");
+                        // $this->climateInstance->backgroundLightGreen()->bold()->black()->out("$vhostPath/*.$brace");
+                        if(empty($files)) {
+                            $this->climateInstance->bold()->red()->out("No config files found in ".$vhostPath);
+                            exit();
+                        }
+                        $input = $this->climateInstance->radio('Please send me one of the following:', $files);
+                        $vhostFile = $input->prompt();
+                        if(file_exists($vhostFile)) {
+                            $vhostContent = file_get_contents($vhostFile);
+                            //Replace vars in template
+                            //MAKE THIS A METHOD
+                            preg_match_all("/{{[a-zA-Z_\s]+}}/", $vhostContent, $matches);
+                            // die(var_dump($matches));
+                            /*
+                            *array(1) {
+  [0]=>
+  array(7) {
+    [0]=>
+    string(23) "{{ VHOST_SERVERADMIN }}"
+    [1]=>
+    string(25) "{{ VHOST_LOCAL_ADDRESS }}"
+    [2]=>
+    string(25) "{{ VHOST_LOCAL_ADDRESS }}"
+    [3]=>
+    string(25) "{{ VHOST_DOCUMENT_ROOT }}"
+    [4]=>
+    string(25) "{{ VHOST_DOCUMENT_ROOT }}"
+    [5]=>
+    string(21) "{{ VHOST_ERROR_LOG }}"
+    [6]=>
+    string(22) "{{ VHOST_ACCESS_LOG }}"
+  }
+}
+
+                             */
+                            if(is_array($matches)) {
+                                foreach ($matches as $value) {
+
+                                }
+                            }
+                        }
+                        // return $response;
+                        // die($vhostFile);
+                break;
+                case 'nginx':
+                break;
+                default:
+                    $this->climateInstance->bold()->red()->out('Web server : '.$values['APP_WEBSERVER'].' not known');
+                break;
+            }
+        }
+
+
         //Edit /etc/hosts
         //a2ensite
         //symlink dev folder and apache RootDirectory folder  /var/www/uwithi/public
+        //reload apache
 
         return false;
     }
